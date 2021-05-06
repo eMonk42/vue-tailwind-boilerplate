a349935ce03e9e06818a04c13390b348e0973890
eMonk42 <lukas.dendtelgmx.de>
2021-05-06 09:10:02 +0200

    initialization

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid place-items-center min-h-screen">
+    <h1 class="text-6xl text-purple-800 font-semibold">Welcome to Spark</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Dashboard",
+});
+</script>
